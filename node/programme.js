0554187423447e510c9382ba695f5ea4387568fb
Patiopatio <buczkowska.p@gmail.com>
2018-05-21 23:47:28 +0200

    Added missing folder

---
+++ node/programme.js
@@ -0,0 +1,9 @@
+// Please print a hello world to the console :D
+// .log --> is actually a function. So a function takes always brackets at the end.
+
+// if you want to startg your programme --> be in the same folder of the file and use: node programme.js
+
+console.log("hello world");
+
+
+// Please commit
